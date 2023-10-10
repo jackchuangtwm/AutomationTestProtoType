a87fe2c2760cf2a5c1becc06274c4b03a0c6210c
RupertGithub <AWS3DEMO@gmail.com>
2023-10-10 14:57:21 +0800

    mod

--- test_api/conftest.py
+++ test_api/conftest.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import os
 import requests
 import logging
-from test_data.get_data_from_excel import ExcelSheetOperator
+
 
 if 'ENV_FILE' in os.environ:
     env_file = os.environ['ENV_FILE']
@@ -11,8 +11,6 @@ if 'ENV_FILE' in os.environ:
 else:
     load_dotenv()
 
-excel_reader = ExcelSheetOperator(file_name='test_data/test_data.xlsx', sheet_name='Login_info_data')
-login_info_dict_list = excel_reader.to_dict_list()
 
 @pytest.fixture()
 def create_session():
@@ -22,17 +20,19 @@ def create_session():
 
 
 @pytest.fixture()
-def login_api(create_session, worker_id):
+def login_api(create_session, request, worker_id):
     session = create_session
 
     logging.info(f'worker_id : {worker_id}')
 
-    account, password, return_code = (
-        login_info_dict_list[int(worker_id.split("w")[1])]['Account'],
-        login_info_dict_list[int(worker_id.split("w")[1])]['Password'],
-        login_info_dict_list[int(worker_id.split("w")[1])]['Return code'],
-    )
+    account, password, return_code = request.param
+    password = os.getenv(password)
+    return_code = int(return_code)
+
+    # if worker_id == 'gw0':
+    #     account = 'HomeSimpsons'
+
     login_response = session.get(url=f'{os.getenv("API_DOMAIN")}/{account}/{password}')
     
     assert int(login_response.status_code) == int(return_code), f'Expected login response code : {return_code}, got : {login_response.status_code}'
-    yield
+    yield login_response
--- test_api/test_para_api.py
+++ test_api/test_para_api.py
@@ -1,2 +1,15 @@
+from test_data.get_data_from_excel import ExcelSheetOperator
+import pytest
+
+excel_reader = ExcelSheetOperator(file_name='test_data/test_data.xlsx', sheet_name='Login_info_data')
+login_info_dict_list = excel_reader.to_dict_list()
+
+@pytest.mark.parametrize(
+        argnames='login_api',
+        argvalues=[info.values() for info in login_info_dict_list],
+        indirect=['login_api']
+)
+
 def test_login(login_api):
-    pass
+    login_response = login_api
+    

38b88f79a40392a73021be7f3d80985a6785ea11
JackChuang <JackChunag@taiwanmobile.com>
2024-03-02 13:25:49 +0000

    reopen install srequiremen

--- exec_test.bat
+++ exec_test.bat
@@ -1,4 +1,4 @@
 del /S /Q results\*
 virtualenv venv
-REM .\venv\Scripts\pip.exe install -r requirements.txt --target=.\venv\Lib\site-packages --upgrade
+.\venv\Scripts\pip.exe install -r requirements.txt --target=.\venv\Lib\site-packages --upgrade
 .\venv\Scripts\python.exe -m pytest -n 2  --alluredir=./results --reruns 3 -p no:faulthandler -k "test" --tb=long
